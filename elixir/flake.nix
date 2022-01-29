7623406a33cced8f572f3344d45853cc32e476c2
IDF31 <ardek66@tutanota.com>
2022-01-29 16:17:19 +0200

    Elixir shell with local mix

--- elixir/flake.nix
+++ elixir/flake.nix
@@ -19,5 +19,19 @@
         };
 
         defaultPackage = self.packages.${sys}.dummy;
+
+        devShell = pkgs.mkShell {
+          buildInputs = [ beamPkgs.elixir_1_13 ];
+          shellHook = ''
+            mkdir -p .nix-mix .nix-hex
+            export MIX_HOME=$PWD/.nix-mix
+            export HEX_HOME=$PWD/.nix-mix
+            export PATH=$MIX_HOME/bin:$HEX_HOME/bin:$PATH
+            mix local.hex --if-missing
+            export LANG=en_US.UTF-8
+            export ERL_AFLAGS="-kernel shell_history enabled"
+          '';
+        };
+
       });
 }

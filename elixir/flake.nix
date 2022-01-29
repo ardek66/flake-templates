e632d6bcd97df38d75597f67249a2e5109818d16
IDF31 <ardek66@tutanota.com>
2022-01-29 14:14:15 +0200

    Fix elixir template

--- elixir/flake.nix
+++ elixir/flake.nix
@@ -15,23 +15,9 @@
           pname = "dummy";
           version = "0.1.0";
           src = ./.;
-          mixEnv = "prod";
           buildInputs = [ beamPkgs.elixir_1_13 ];
         };
 
         defaultPackage = self.packages.${sys}.dummy;
-
-        devShell = pkgs.mkShell {
-          shellHook = ''
-              # this allows mix to work on the local directory
-              export MIX_HOME=$PWD/.nix-mix
-              export HEX_HOME=$PWD/.nix-mix
-              export PATH=$MIX_HOME/bin:$HEX_HOME/bin:$PATH
-              # TODO: not sure how to make hex available without installing it afterwards.
-              mix local.hex --if-missing
-              export LANG=en_US.UTF-8
-              export ERL_AFLAGS="-kernel shell_history enabled"
-              '';
-        };
       });
 }

6de91fa9285e282b8f28cff1d4a2d70a25e0c0e6
IDF31 <ardek66@tutanota.com>
2022-06-16 13:27:56 +0300

    Improve nim tempalte

--- nim/flake.nix
+++ nim/flake.nix
@@ -8,17 +8,26 @@
   
   outputs = { self, nixpkgs, flake-utils, flake-nimble }:
     flake-utils.lib.eachDefaultSystem (sys:
-      let
-        pkgs = nixpkgs.legacyPackages.${sys};
-        nimblePkgs = flake-nimble.packages.${sys};
-      in {
-        packages.dummy = pkgs.nimPackages.buildNimPackage {
-          pname = "dummy";
-          version = "0.1.0";
-          src = ./.;
-          buildInputs = [ ];
+      let pkgs = nixpkgs.legacyPackages.${sys}; in
+      rec {
+
+        pkgsWithNimble = pkgs.appendOverlays [ flake-nimble.overlay ];
+        
+        packages = flake-utils.lib.flattenTree {
+          nimlsp = pkgs.nimlsp;
+          
+          dummy = pkgs.nimPackages.buildNimPackage {
+            pname = "dummy";
+            version = "0.1.0";
+            src = ./.;
+            buildInputs = [ ];
+          };
         };
 
-        defaultPackage = self.packages.${sys}.dummy;
+        defaultPackage = packages.dummy;
+
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with packages; [ nimlsp ];
+        };
       });
 }
